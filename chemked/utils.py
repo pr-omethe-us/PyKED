2dec2086aaca3e058388a64876f02b7e0b7df9ab
Bryan W. Weber <bryan@bryanwweber.com>
2016-08-19 14:04:53 -0400

    Abstract out the common schema types

--- chemked/utils.py
+++ chemked/utils.py
@@ -13,28 +13,80 @@ units = pint.UnitRegistry()
 
 units.define('cm3 = centimeter**3')
 
+
+def merge_dicts(*dict_args):
+    '''
+    Given any number of dicts, shallow copy and merge into a new dict,
+    precedence goes to key value pairs in later dicts. From http://stackoverflow.com/a/26853961
+    '''
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
 get_temp_unit = {'K': 'kelvin',
                  'C': 'degC',
                  'F': 'degF',
                  'R': 'degR'
                  }
 
-# YAML schema for ChemKED YAML files
-schema = {
-    'file-author': {'type': 'dict', 'required': True, 'schema': {  # TODO: DRY about the attributes a person can have
+value_unit_schema = {
+    'type': 'dict',
+    'schema': {
+        'value': {'type': 'float', 'min': 0.0, 'required': True},  # TODO: Write unit validator
+        'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
+        },
+    }
+
+pressure_schema = value_unit_schema
+temperature_schema = value_unit_schema
+ignition_delay_schema = value_unit_schema
+compression_time_schema = value_unit_schema
+
+composition_schema = {
+    'type': 'list',
+    'schema': {
+        'type': 'dict',
+        'schema': {
+            'species': {'type': 'string', 'required': True},
+            'InChI': {'type': 'string', 'required': True},  # TODO: Write InChI validator
+            'mole-fraction': {'type': 'float', 'required': True},  # TODO: Allow mass fraction
+            },
+        },
+    }
+
+ignition_type_schema = {
+    'type': 'dict',
+    'schema': {
+        'target': {'type': 'string', 'required': True},
+        'type': {'type': 'string', 'allowed': ['d/dt max', 'max', 'min'], 'required': True}
+        },
+    }
+
+pressure_rise_schema = {
+    'type': 'dict',
+    'schema': {
+        'value': {'type': 'float', 'required': True, 'min': 0.0},  # TODO: Write unit validator
+        'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
+        },
+    }
+
+person_schema = {
+    'type': 'dict',
+    'schema': {
         'name': {'type': 'string', 'required': True},
         'ORCID': {'type': 'string'},  # TODO: Write ORCID validator
         }
-    },
+    }
+
+# YAML schema for ChemKED YAML files
+schema = {
+    'file-author': merge_dicts(person_schema, dict(required=True)),
     'file-version': {'type': 'string', 'required': True},
     'chemked-version': {'type': 'string', 'required': True, 'allowed': '0.0.1'},  # TODO: Implement proper version comparison
     'reference': {'type': 'dict', 'required': True, 'schema': {
         'doi': {'type': 'string'},  # TODO: Write DOI validator
-        'authors': {'type': 'list', 'schema': {'type': 'dict', 'schema': {
-            'name': {'type': 'string', 'required': True},
-            'ORCID': {'type': 'string'},
-            },
-        }},
+        'authors': {'type': 'list', 'required': True, 'schema': person_schema},
         'journal': {'type': 'string', 'required': True},
         'year': {'type': 'integer', 'min': 1600, 'max': now.year + 1, 'required': True},
         'volume': {'type': 'integer', 'min': 0, 'required': True},
@@ -43,65 +95,26 @@ schema = {
         },
     },
     'apparatus': {'type': 'dict', 'required': True, 'schema': {
-        'kind': {'type': 'string', 'required': True, 'allowed': ['shock tube', 'rapid compression machine']},
+        'kind': {'type': 'string', 'required': True,
+                 'allowed': ['shock tube', 'rapid compression machine']},
         'institution': {'type': 'string'},
         'facility': {'type': 'string'},
         },
     },
-    'common-properties': {'type': 'dict', 'schema': {  # TODO: DRY with these options
-        'pressure': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'min': 0.0, 'required': True},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
+    'common-properties': {'type': 'dict', 'schema': {
+        'pressure': pressure_schema,
+        'composition': composition_schema,
+        'ignition-type': ignition_type_schema,
+        'pressure-rise': pressure_rise_schema,
         },
-        'composition': {'type': 'list', 'schema': {'type': 'dict', 'schema': {
-            'species': {'type': 'string', 'required': True},
-            'InChI': {'type': 'string', 'required': True},  # TODO: Write InChI validator
-            'mole-fraction': {'type': 'float', 'required': True},  # TODO: Allow mass fraction
-            },
-        }},
-        'ignition-type': {'type': 'dict', 'schema': {
-            'target': {'type': 'string', 'required': True},
-            'type': {'type': 'string', 'allowed': ['d/dt max', 'max', 'min'], 'required': True}
-            },
-        },
-        'pressure-rise': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'required': True, 'min': 0.0},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
-        },
-    }},
+    },
     'datapoints': {'type': 'list', 'required': True, 'schema': {'type': 'dict', 'schema': {
-        'temperature': {'type': 'dict', 'required': True, 'schema': {
-            'value': {'type': 'float', 'required': True, 'min': 0.0, 'max': 5000.0},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            }},
-        'ignition-delay': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'required': True, 'min': 0.0},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
-        },
-        'pressure': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'min': 0.0, 'required': True},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
-        },
-        'composition': {'type': 'list', 'schema': {'type': 'dict', 'schema': {
-            'species': {'type': 'string', 'required': True},
-            'InChI': {'type': 'string', 'required': True},  # TODO: Write InChI validator
-            'mole-fraction': {'type': 'float', 'required': True},  # TODO: Allow mass fraction
-            },
-        }},
-        'ignition-type': {'type': 'dict', 'schema': {
-            'target': {'type': 'string', 'required': True},
-            'type': {'type': 'string', 'allowed': ['d/dt max', 'max', 'min'], 'required': True}
-            },
-        },
-        'compression-time': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'required': True, 'min': 0.0},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
-        },
+        'temperature': temperature_schema,
+        'ignition-delay': ignition_delay_schema,
+        'pressure': pressure_schema,
+        'composition': composition_schema,
+        'ignition-type': ignition_type_schema,
+        'compression-time': compression_time_schema,
         'volume-history': {'type': 'dict', 'schema': {
             'time': {'type': 'dict', 'required': True, 'schema': {
                 'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
@@ -113,14 +126,17 @@ schema = {
                 'column': {'type': 'integer', 'required': True},
                 },
             },
-            'values': {'type': 'list', 'schema': {'type': 'list', 'items': [{'type': 'float'}, {'type': 'float'}]}},
+            'values': {
+                'type': 'list',
+                'schema': {
+                    'type': 'list',
+                    'items': [{'type': 'float'}, {'type': 'float'}]
+                    },
+                'required': True
+                },
             },
         },
-        'pressure-rise': {'type': 'dict', 'schema': {
-            'value': {'type': 'float', 'required': True, 'min': 0.0},  # TODO: Write unit validator
-            'units': {'type': 'string', 'required': True},  # TODO: Write unit validator
-            },
-        },
+        'pressure-rise': pressure_rise_schema,
     }},
     },
     'experiment-type': {'type': 'string', 'required': True, 'allowed': ['ignition delay']},
