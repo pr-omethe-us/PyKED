e53924e782317921455685615140864ee5026606
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2017-02-27 12:27:34 -0500

    :ambulance: Make pressure optional in common-properties
    Pressure is still required in datapoints, but should not be required
    in common-properties since it may change between points.
    
    Fixes #36

--- pyked/validation.py
+++ pyked/validation.py
@@ -32,12 +32,12 @@ schema_file = pkg_resources.resource_filename(__name__, 'chemked_schema.yaml')
 with open(schema_file, 'r') as f:
     schema = yaml.safe_load(f)
 
-# These top-level keys in the schema server as references for lower-level keys.
+# These top-level keys in the schema serve as references for lower-level keys.
 # They are removed to prevent conflicts due to required variables, etc.
 for key in ['author', 'value-unit-required', 'value-unit-optional',
             'composition', 'ignition-type'
             ]:
-    schema.pop(key)
+    del schema[key]
 
 # SI units for available value-type properties
 property_units = {'temperature': 'kelvin',
