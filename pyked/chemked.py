152d9891140da12675209381454f456407bbbb41
Bryan W. Weber <bryan.w.weber@gmail.com>
2017-03-31 14:46:43 -0400

    Update docstrings formatting

--- pyked/chemked.py
+++ pyked/chemked.py
@@ -13,9 +13,26 @@ from .validation import schema, OurValidator, yaml
 from .utils import Q_
 
 VolumeHistory = namedtuple('VolumeHistory', ['time', 'volume'])
+VolumeHistory.__doc__ = 'Time history of the volume in an RCM experiment'
+VolumeHistory.time.__doc__ = '(`~numpy.ndarray`): the time during the experiment'
+VolumeHistory.volume.__doc__ = '(`~numpy.ndarray`): the volume during the experiment'
+
 Reference = namedtuple('Reference',
                        ['volume', 'journal', 'doi', 'authors', 'detail', 'year', 'pages'])
+Reference.__doc__ = 'Information about the article or report where the data can be found'
+Reference.volume.__doc__ = '(`str`) The journal volume'
+Reference.journal.__doc__ = '(`str`) The name of the journal'
+Reference.doi.__doc__ = '(`str`) The Digital Object Identifier of the article'
+Reference.authors.__doc__ = '(`list`) The list of authors of the article'
+Reference.detail.__doc__ = '(`str`) Detail about where the data can be found in the article'
+Reference.year.__doc__ = '(`str`) The year the article was published'
+Reference.pages.__doc__ = '(`str`) The pages in the journal where the article was published'
+
 Apparatus = namedtuple('Apparatus', ['kind', 'institution', 'facility'])
+Apparatus.__doc__ = '(`str`) Information about the experimental apparatus used to generate the data'
+Apparatus.kind.__doc__ = '(`str`) The kind of experimental apparatus'
+Apparatus.institution.__doc__ = '(`str`) The institution where the experiment is located'
+Apparatus.facility.__doc__ = '(`str`) The particular experimental facility at the location'
 
 
 class ChemKED(object):
@@ -26,21 +43,22 @@ class ChemKED(object):
     the the reference information, versions, and file author.
 
     Args:
-        yaml_file (str, optional): The filename of the YAML database in ChemKED format.
-        dict_input (str, optional): A dictionary with the parsed ouput of YAML file in ChemKED
+        yaml_file (`str`, optional): The filename of the YAML database in ChemKED format.
+        dict_input (`str`, optional): A dictionary with the parsed ouput of YAML file in ChemKED
             format.
 
     Attributes:
-        datapoints (list): List of `DataPoint` objects storing each datapoint in the database.
-        reference (namedtuple): Attributes include ``volume``, ``journal``, ``doi``, ``authors``,
-            ``detail``, ``year``, and ``pages`` describing the reference from which the datapoints
-            are derived.
-        apparatus (namedtuple): Attributes include ``kind`` of experimental apparatus, and the
-            ``institution`` and ``facility`` where the experimental apparatus is located.
-        chemked_version (str): Version of the ChemKED database schema used in this file.
-        experiment_type (str): Type of exeperimental data contained in this database.
-        file_author (dict): Information about the author of the ChemKED database file.
-        file_version (str): Version of the ChemKED database file.
+        datapoints (`list`): List of `DataPoint` objects storing each datapoint in the database.
+        reference (`~collections.namedtuple`): Attributes include ``volume``, ``journal``, ``doi``,
+            ``authors``, ``detail``, ``year``, and ``pages`` describing the reference from which the
+            datapoints are derived.
+        apparatus (`~collections.namedtuple`): Attributes include ``kind`` of experimental
+            apparatus, and the ``institution`` and ``facility`` where the experimental apparatus is
+            located.
+        chemked_version (`str`): Version of the ChemKED database schema used in this file.
+        experiment_type (`str`): Type of exeperimental data contained in this database.
+        file_author (`dict`): Information about the author of the ChemKED database file.
+        file_version (`str`): Version of the ChemKED database file.
     """
     def __init__(self, yaml_file=None, dict_input=None):
         if yaml_file is not None:
@@ -80,10 +98,10 @@ class ChemKED(object):
         """Validate the parsed YAML file for adherance to the ChemKED format.
 
         Arguments:
-            properties (dict): Dictionary created from the parsed YAML file
+            properties (`dict`): Dictionary created from the parsed YAML file
 
         Raises:
-            ValueError: If the YAML file cannot be validated, a ``ValueError`` is raised whose
+            `ValueError`: If the YAML file cannot be validated, a `ValueError` is raised whose
                 string contains the errors that are present.
         """
         validator = OurValidator(schema)
@@ -99,32 +117,36 @@ class ChemKED(object):
         """Get a Pandas DataFrame of the datapoints in this instance.
 
         Arguments:
-            output_columns (list, optional): List of strings specifying the columns to include
-                in the output DataFrame. The default is ``None``, which outputs all of the
+            output_columns (`list`, optional): List of strings specifying the columns to include
+                in the output DataFrame. The default is `None`, which outputs all of the
                 columns. Options include (not case sensitive):
-                    * Temperature
-                    * Pressure
-                    * Ignition Delay
-                    * Composition
-                    * Equivalence Ratio
-                    * Reference
-                    * Apparatus
-                    * Experiment Type
-                    * File Author
-                    * File Version
-                    * ChemKED Version
-                In addition, specific fields from the Reference and Apparatus attributes can
+
+                    * ``Temperature``
+                    * ``Pressure``
+                    * ``Ignition Delay``
+                    * ``Composition``
+                    * ``Equivalence Ratio``
+                    * ``Reference``
+                    * ``Apparatus``
+                    * ``Experiment Type``
+                    * ``File Author``
+                    * ``File Version``
+                    * ``ChemKED Version``
+
+                In addition, specific fields from the ``Reference`` and ``Apparatus`` attributes can
                 be included by specifying the name after a colon. These options are:
-                    * Reference:Volume
-                    * Reference:Journal
-                    * Reference:DOI
-                    * Reference:Authors
-                    * Reference:Detail
-                    * Reference:Year
-                    * Reference:Pages
-                    * Apparatus:Kind
-                    * Apparatus:Facility
-                    * Apparatus:Institution
+
+                    * ``Reference:Volume``
+                    * ``Reference:Journal``
+                    * ``Reference:DOI``
+                    * ``Reference:Authors``
+                    * ``Reference:Detail``
+                    * ``Reference:Year``
+                    * ``Reference:Pages``
+                    * ``Apparatus:Kind``
+                    * ``Apparatus:Facility``
+                    * ``Apparatus:Institution``
+
                 Only the first author is printed when ``Reference`` or ``Reference:Authors`` is
                 selected because the whole author list may be quite long.
 
@@ -133,7 +155,8 @@ class ChemKED(object):
             >>> df = ChemKED(yaml_file).get_dataframe(['Temperature', 'Ignition Delay'])
 
         Returns:
-            DataFrame: Contains the information regarding each point in the ``datapoints`` attribute
+            `~pandas.DataFrame`: Contains the information regarding each point in the ``datapoints``
+                attribute
         """
         import pandas as pd
 
@@ -202,23 +225,23 @@ class ChemKED(object):
 class DataPoint(object):
     """Class for a single datapoint.
 
-    The DataPoint class stores the information associated with a single data point in the dataset
-    parsed from the ChemKED YAML input.
+    The `DataPoint` class stores the information associated with a single data point in the dataset
+    parsed from the `ChemKED` YAML input.
 
     Arguments:
-        properties (dict): Dictionary adhering to the ChemKED format for ``datapoints``
+        properties (`dict`): Dictionary adhering to the ChemKED format for ``datapoints``
 
     Attributes:
-        composition (list): List of dictionaries representing the species and their quantities
+        composition (`list`): List of dictionaries representing the species and their quantities
         ignition_delay (pint.Quantity): The ignition delay of the experiment
         temperature (pint.Quantity): The temperature of the experiment
         pressure (pint.Quantity): The pressure of the experiment
         pressure_rise (pint.Quantity, optional): The amount of pressure rise during the induction
             period of a shock tube experiment.
         compression_time (pint.Quantity, optional): The compression time for an RCM experiment.
-        ignition_type (dict): Dictionary with the ignition target and type.
-        volume_history (namedtuple, optional): The volume history of the reactor during an RCM
-            experiment.
+        ignition_type (`dict`): Dictionary with the ignition target and type.
+        volume_history (`~collections.namedtuple`, optional): The volume history of the reactor
+            during an RCM experiment.
     """
     def __init__(self, properties):
         for prop in ['ignition-delay', 'temperature', 'pressure', 'pressure-rise',
@@ -268,7 +291,7 @@ class DataPoint(object):
         """Get the composition in a string format suitable for input to Cantera.
 
         Returns:
-            str: String in the ``SPEC: AMT, SPEC: AMT`` format
+            `str`: String in the ``SPEC: AMT, SPEC: AMT`` format
         """
         if self.composition_type == 'mole-fraction':
             return ', '.join(['{!s}: {:.4e}'.format(c['species-name'], c['mole-fraction']) for c in
@@ -284,7 +307,7 @@ class DataPoint(object):
         """Get the composition in a string format suitable for input to Cantera.
 
         Returns:
-            str: String in the ``SPEC: AMT, SPEC: AMT`` format
+            `str`: String in the ``SPEC: AMT, SPEC: AMT`` format
         """
         if self.composition_type == 'mass-fraction':
             return ', '.join(['{!s}: {:.4e}'.format(c['species-name'], c['mass-fraction']) for c in
