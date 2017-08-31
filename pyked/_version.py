a313f88ddd47f21239ad59ed029e1d2370ba6fc4
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2017-08-31 13:12:53 +0100

    bump version slightly

--- pyked/_version.py
+++ pyked/_version.py
@@ -1,4 +1,4 @@
-__version_info__ = (0, 2, 0, '')
+__version_info__ = (0, 2, 0, 'a1')
 __version__ = '.'.join(map(str, __version_info__[:3]))
 if len(__version_info__) == 4:
     __version__ += __version_info__[-1]
